a53da5eb75fa3885d41b5cffca1825e7ba1a459d
Denis Solovjov <d.salauyou@invatechs.com>
2023-10-29 17:53:23 +0100

    add init layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,60 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import SwitchItem from '../components/SwitchItem.vue'
+import IconMenu from '../components/icons/IconMenu.vue'
+
+const items: Array<any> = [{ title: 'Table Light 01' }, { title: 'Table Light 02' }, { title: 'Table Light 03' }, { title: 'Table Light 04' }, { title: 'Table Light 05' }, { title: 'Table Light 06' }]
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
+    <ul class="switch-wrapper">
+
+      <li v-for="({ title }, index) in items" :key="index">
+        <div class="header">
+          <h2>{{ title }}</h2>
+          <IconMenu></IconMenu>
+        </div>
+        <SwitchItem :id="index.toString()"></SwitchItem>
+      </li>
+    </ul>
   </main>
 </template>
+
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: flex-start;
+  gap: 30px;
+  flex-wrap: wrap;
+
+  li {
+    .header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 15px;
+
+      h2 {
+        color: #FFF;
+        font-family: Inter;
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+      }
+    }
+
+    border-radius: 4px;
+    border: 2px solid rgba(255, 255, 255, 0.2);
+    padding: 20px;
+  }
+
+}
+</style>
